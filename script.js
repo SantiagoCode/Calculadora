7b3332d259bf3be20fd3e29290d6720dec53f28b
Santiago <santiagosalazar.dev@gmail.com>
2013-01-01 01:46:06 -0430

    version 2.0

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var btn_calcular = document.getElementById('btn_calc');
 var cambio = document.getElementById("operador");
 var txt_resultados = document.getElementById('contenedor_resultados');
+var btn_space = document.getElementById("btn_space");
 var operacion = document.getElementById('actualizar');
 var contador = 1;
 var signo = "+";
+var last_value;
 
 btn_calcular.addEventListener("click", calcular);
 cambio.addEventListener("change", actualizar);
+
+
+
 
 // aqui actualizamos los valores de X y Y
 function calcular()
@@ -45,13 +50,30 @@ function historial(x, y, result)
 {
   txt_resultados.innerHTML +=
   "<strong> Operacion " + contador + ":</strong> <br />" +
-  x + " " + cambio.value + " " + y + " = " + result + ". <br />";
+  x + " " + cambio.value + " " + y + " = " + result + "<br />";
+
+  last_value = result;
 
   contador++;
+
+  btn_space.innerHTML =
+  "<button class='select' type='button' name='button'>Usar último valor</button>"
+
+  btn_space.addEventListener("click", reusar);
 }
+
+
 
 // Asi actualizarmos la interfaz
 function actualizar()
 {
   operacion.innerHTML = cambio.value;
+}
+
+
+// asi permitimos continuar el calculo
+function reusar()
+{
+  x.value = parseInt(last_value);
+  y.value = 0;
 }
