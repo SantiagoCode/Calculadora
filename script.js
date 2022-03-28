b164df9ce99d9b8a22ed5ba4e5cc22a1699ff364
Santiago Salazar <santiagosalazar.dev@gmail.com>
2022-03-28 15:20:52 -0400

    Hola, richar

--- script.js
+++ script.js
@@ -1,83 +1,88 @@
-let btn_calcular = document.getElementById('btn_calc');
-let cambio = document.getElementById("operador");
-let tercer_cuadro = document.querySelector(".resultados");
-let txt_resultados;
-let btn_space;
-let operacion = document.getElementById('actualizar');
-let contador = 1;
-let signo = "+";
-let last_value;
+class Calculadora {
+    constructor(){
+        this.btn_calcular = document.getElementById('btn_calc');
+        this.cambio = document.getElementById("operador");
+        this.cambio_value = this.cambio.value;
+        this.tercer_cuadro = document.querySelector(".resultados");
+        this.operacion = document.getElementById('actualizar');
+        this.contador = 1;
+        this.signo = "+";
+        this.last_value;
+        this.txt_resultados;
+        this.btn_space;
+    }
+    
+    // aqui actualizamos los valores de X y Y
+    calcular () {
+        let x = parseInt( document.getElementById('x').value );
+        let y = parseInt( document.getElementById('y').value );
 
-btn_calcular.addEventListener("click", calcular);
-cambio.addEventListener("change", actualizar);
+        this.operar(x, y);
+    }
 
 
+    // aqui identificamos si es uns suma o resta
+    operar = (x, y) => {
+        switch (this.cambio_value) {
+            case "+":
+            var result = x + y;
+            break;
+            case "-":
+            var result = x - y;
+            break;
+            case "x":
+            var result = x * y;
+            break;
+            case "/":
+            var result = x / y;
+            break;
+            default:
+        }
+
+        historial(x, y, result);
+    }
+
+    // aqui creamos un historial de operaciones
+    historial = (x, y, result) => {
+        if(contador == 1){
+            tercer_cuadro.innerHTML = 
+            "<h2>Resultados: </h2> <p id='contenedor_resultados'></p> <p id='btn_space'></p>";
+
+            txt_resultados = document.getElementById('contenedor_resultados');
+            btn_space = document.getElementById("btn_space");
+        }
+
+        txt_resultados.innerHTML +=
+        "<strong> Operacion " + contador + ":</strong> <br />" +
+        x + " " + cambio.value + " " + y + " = " + result + "<br />";
+
+        last_value = result;
+
+        contador++;
+
+        btn_space.innerHTML =
+        "<button class='select act' type='button' name='button'>Usar último valor</button>"
+
+        btn_space.addEventListener("click", reusar);
+    }
+
+    
+    // Asi actualizarmos la interfaz
+    actualizar () {
+        operacion.innerHTML = cambio.value;
+    }
 
 
-// aqui actualizamos los valores de X y Y
-function calcular () {
-  let x = parseInt( document.getElementById('x').value );
-  let y = parseInt( document.getElementById('y').value );
-
-  operar(x, y);
+    // asi permitimos continuar el calculo
+    reusar = () => {
+        x.value = parseInt(last_value);
+        y.value = 0;
+    }
 }
 
-// aqui identificamos si es uns suma o resta
-const operar = (x, y) => {
-  switch (cambio.value) {
-    case "+":
-      var result = x + y;
-      break;
-    case "-":
-      var result = x - y;
-      break;
-    case "x":
-      var result = x * y;
-      break;
-    case "/":
-      var result = x / y;
-      break;
-    default:
-
-  }
-
-  historial(x, y, result);
-}
-
-// aqui creamos un historial de operaciones
-const historial = (x, y, result) => {
-  if(contador == 1){
-    tercer_cuadro.innerHTML = 
-    "<h2>Resultados: </h2> <p id='contenedor_resultados'></p> <p id='btn_space'></p>";
-
-    txt_resultados = document.getElementById('contenedor_resultados');
-    btn_space = document.getElementById("btn_space");
-  }
-
-  txt_resultados.innerHTML +=
-  "<strong> Operacion " + contador + ":</strong> <br />" +
-  x + " " + cambio.value + " " + y + " = " + result + "<br />";
-
-  last_value = result;
-
-  contador++;
-
-  btn_space.innerHTML =
-  "<button class='select act' type='button' name='button'>Usar último valor</button>"
-
-  btn_space.addEventListener("click", reusar);
-}
+let miCalculadora = new Calculadora();
 
 
 
-// Asi actualizarmos la interfaz
-function actualizar () {
-  operacion.innerHTML = cambio.value;
-}
-
-
-// asi permitimos continuar el calculo
-const reusar = () => {
-  x.value = parseInt(last_value);
-  y.value = 0;
-}
+miCalculadora.btn_calcular.addEventListener("click", miCalculadora.calcular);
+miCalculadora.cambio.addEventListener("change", miCalculadora.actualizar);
